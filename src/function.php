f1884d6282c150004a8cdf67dac73d1bfd213fe5
sacleuxa <147699402+sacleuxa@users.noreply.github.com>
2024-01-22 14:10:35 +0100

    delete

--- src/function.php
+++
@@ -1,9 +0,0 @@
-<?php
-    function calculAge($annee){
-        $anneeNaissance = "01-01-".$annee;
-        $aujourdhui = date("Y");
-        $diff = date_diff(date_create($anneeNaissance), date_create($aujourdhui));
-        $age = $diff->format('%y');
-        return $age;
-    }
-?>
